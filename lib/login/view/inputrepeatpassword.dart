d1cbfae7a99a6534e2359d25d26bc452f63c50b6
komeylafkhami <komeylafkhami@gmail.com>
2024-04-17 16:45:13 +0330

    Complete inputrepeatpassword

--- lib/login/view/inputrepeatpassword.dart
+++ lib/login/view/inputrepeatpassword.dart
@@ -10,17 +10,32 @@ class InputRepeatPassword extends StatefulWidget {
 class _InputRepeatPasswordState extends State<InputRepeatPassword> {
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      obscureText: true,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
-          hintText: "Repeat your password",
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: Colors.grey),
-          ),
-          suffixIcon: Icon(Icons.visibility, color: Colors.grey,)
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Repeat Password",
+          textAlign: TextAlign.left,
+          style: TextStyle(color: Colors.black54),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        TextFormField(
+          obscureText: true,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+              hintText: "Repeat your password",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+              suffixIcon: Icon(
+                Icons.visibility,
+                color: Colors.grey,
+              )),
+        )
+      ],
     );
   }
 }
