ad256ff262ca586ca904ab6da28ebb4e7b447881
komeylafkhami <komeylafkhami@gmail.com>
2024-04-17 16:45:49 +0330

    Complete inputpassword

--- lib/login/view/inputpassword.dart
+++ lib/login/view/inputpassword.dart
@@ -10,17 +10,32 @@ class InputPassword extends StatefulWidget {
 class _InputPasswordState extends State<InputPassword> {
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      obscureText: true,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
-          hintText: "Enter your password",
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: Colors.grey),
-          ),
-          suffixIcon: Icon(Icons.visibility, color: Colors.grey,)
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Create Password",
+          textAlign: TextAlign.left,
+          style: TextStyle(color: Colors.black54),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        TextFormField(
+          obscureText: true,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+              hintText: "Enter your password",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+              suffixIcon: Icon(
+                Icons.visibility,
+                color: Colors.grey,
+              )),
+        )
+      ],
     );
   }
 }
